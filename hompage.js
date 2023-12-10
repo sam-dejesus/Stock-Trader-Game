004ea8f270d090ae5f10cb1245ee3e2eee2ef673
samuel de jesus <samuel1de2jesus3@gmail.com>
2023-12-10 18:41:48 -0500

    added the buy button that adds the stock to the local storage

--- hompage.js
+++ hompage.js
@@ -1,23 +1,24 @@
-var stockBtn = document.getElementById("stockBtn");
-var searchedStock = document.getElementById("searchedStock"); // Corrected the ID
 
-stockBtn.addEventListener("click", performSearch); // Changed to performSearch
+var searchedStock = document.getElementById("searchedStock"); 
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    getStocks(); 
+});
 function getStocks() {
-    // Get stocks from local storage
-    const stocksString = localStorage.getItem("stocks");
+  
+    const stocksString = localStorage.getItem("myStocks");
 
     if (stocksString) {
-        // Parse the JSON string to an array
+   
         const stocks = JSON.parse(stocksString);
 
-        // Get the stock list container
+
         const stockListContainer = document.getElementById('stock-list');
 
-        // Clear existing content
+
         stockListContainer.innerHTML = "";
 
-        // Loop through each stock and add it to the list
         stocks.forEach(stock => {
             const listItem = document.createElement('li');
             listItem.textContent = stock;
@@ -27,10 +28,10 @@ function getStocks() {
 }
 
 async function performSearch() {
-    // Get the search term from the input field
+
     var searchTerm = document.getElementById('search-input').value;
 
-    // Construct the URL with the search term
+
     const url = 'https://realstonks.p.rapidapi.com/' + searchTerm;
 
     const options = {
@@ -41,33 +42,41 @@ async function performSearch() {
         }
     };
 
-    // Declare result variable outside the try block
     let result;
 
     try {
-        // Fetch data from the API
+    
         const response = await fetch(url, options);
         console.log(response)
-        result = await response.json(); // Parse response as JSON
+        result = await response.json(); 
         console.log(result);
 
-        // Create elements for displaying stock information
+        
         var stockDiv = document.createElement('div');
         var stockName = document.createElement('h4');
         var stockPrice = document.createElement('p');
         var buyBtn = document.createElement('button')
         buyBtn.innerHTML= "BUY"
-        stockPrice.innerHTML = 'Price: ' + result.price; // Access result properties
+        stockPrice.innerHTML = 'Price: ' + result.price; 
         stockName.innerHTML = 'Name: ' + searchTerm;
 
         stockDiv.appendChild(stockName);
         stockDiv.appendChild(stockPrice);
         stockDiv.classList.add("stockDiv")
       
-        searchedStock.appendChild(stockDiv, buyBtn);
+        searchedStock.appendChild(stockDiv);
+        searchedStock.appendChild(buyBtn);
+
+        stockData = {
+            name: searchTerm,
+            price: result.price}
+        buyBtn.addEventListener("click", ()=>{
+            localStorage.setItem('myStocks', JSON.stringify(stockData));
+        })
+        
     } catch (error) {
         console.error(error);
-        // Handle errors as needed
+        
     }
 }
 
